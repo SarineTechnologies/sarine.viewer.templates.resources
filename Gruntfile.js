5a845accb5a9f0ba5dd64417ba8196db4525320e
aharon.amram <aharon.amram@sarine.com>
2016-05-10 15:57:54 +0300

    add publish grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,6 +80,26 @@ module.exports = function (grunt) {
                 files: {}
             }
         },
+        push: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                releaseBranch: false,
+                add: true,
+                addFiles: ['.'], // '.' for all files except ingored files in .gitignore
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                npm: false,
+                npmTag: 'Release v%VERSION%',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+            }
+        },
         appcache: {
             options: {
                 // Task-specific options go here.
